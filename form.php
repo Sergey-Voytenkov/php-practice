bb21de0fd15705795b02723afe1af7b397a087f8
SergeyV <sergey101.eco@gmail.com>
2019-09-28 10:43:08 -0400

    9/28/19

--- form.php
+++ form.php
@@ -1,8 +1,23 @@
 <html>
 	<body>
+		<p><strong>Lastname: </strong><?php echo $_POST["lastname"]; ?></p>
+		<p><strong>Firstname: </strong><?php echo $_POST["firstname"]; ?></p>
+		<p><strong>Email: </strong><?php echo $_POST["email"]; ?></p>
 		<?php 
-			print_r($_POST);
-			echo 'Hello Serge';
+			if(isset($_POST["gender"]))echo "<p><strong>Gender: </strong>" . $_POST["gender"] . "</p>";
+		?>
+		<p><strong>School: </strong><?php echo $_POST["school"]; ?></p>
+		<p><strong>Subjects: </strong><?php print_r($_POST["subject"]); ?></p>
+		<p><strong>About Me: </strong><br/><?php echo $_POST["aboutme"]; ?></p>
+		<?php 
+			if(isset($_POST["fchecker"])) {
+				$filename = $_POST["fchecker"];
+				if (file_exists($filename)) {
+					echo "The file $filename exists";
+				} else {
+					echo "The file $filename does not exist";
+				}
+			}
 		?>
 	</body>
 </html>
