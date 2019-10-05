f153496dfab53b72c9be47d8d41af948d839cd67
SergeyV <sergey101.eco@gmail.com>
2019-10-04 20:58:04 -0400

    Just did the work asked of me!

--- form.php
+++ form.php
@@ -4,19 +4,21 @@
 		<p><strong>Firstname: </strong><?php echo $_POST["firstname"]; ?></p>
 		<p><strong>Email: </strong><?php echo $_POST["email"]; ?></p>
 		<?php 
-			if(isset($_POST["gender"]))echo "<p><strong>Gender: </strong>" . $_POST["gender"] . "</p>";
-		?>
-		<p><strong>School: </strong><?php echo $_POST["school"]; ?></p>
-		<p><strong>Subjects: </strong><?php print_r($_POST["subject"]); ?></p>
-		<p><strong>About Me: </strong><br/><?php echo $_POST["aboutme"]; ?></p>
-		<?php 
+			if(isset($_POST["gender"])) echo "<p><strong>Gender: </strong>" . $_POST["gender"] . "</p>";
+		
+			if(isset($_POST["school"])) echo "<p><strong>School: </strong>" . $_POST["school"] . "</p>";
+		
+			if(isset($_POST["subject"])) echo "<p><strong>Subjects: </strong>" . $_POST["subject"] . "</p>";
+		
+			if(isset($_POST["aboutme"])) echo "<p><strong>About Me: </strong>" . $_POST["aboutme"] . "</p>";
+		
 			if(isset($_POST["fchecker"])) {
 				$filename = $_POST["fchecker"];
 				if (file_exists($filename)) {
-					echo "The file $filename exists";
-				} else {
-					echo "The file $filename does not exist";
-				}
+						echo "The file $filename exists";
+					} else {
+						echo "The file $filename does not exist";
+					}
 			}
 		?>
 	</body>
