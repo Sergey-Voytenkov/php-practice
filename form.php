5716448ab7a792f13b9f966719db76eb3d9f1561
SergeyV <sergey101.eco@gmail.com>
2019-10-22 19:26:56 -0400

    Need help, it prints only 1's every time.

--- form.php
+++ form.php
@@ -1,35 +1,20 @@
 <html>
 	<body>
-		<p><strong>Lastname: </strong><?php echo $_POST["lastname"]; ?></p>
-		<p><strong>Firstname: </strong><?php echo $_POST["firstname"]; ?></p>
-		<p><strong>Email: </strong><?php echo $_POST["email"]; ?></p>
 		<?php 
-			if(isset($_POST["gender"])) echo "<p><strong>Gender: </strong>" . $_POST["gender"] . "</p>";
-		
-			if(isset($_POST["school"])) echo "<p><strong>School: </strong>" . $_POST["school"] . "</p>";
-		
-			if(isset($_POST["subject"])) echo "<p><strong>Subjects: </strong>" . $_POST["subject"] . "</p>";
-		
-			if(isset($_POST["aboutme"])) echo "<p><strong>About Me: </strong>" . $_POST["aboutme"] . "</p>";
-		
 			if(isset($_POST["fchecker"])) {
 				$filename = $_POST["fchecker"];
 				if (file_exists($filename)) {						
 					$handle = fopen($filename, 'r');
-					while (fgets($handle) != "") {
-						echo htmlspecialchars(fgets($handle, 1));
+					while ($buffer = (fgets($handle)) !== false) {
+						echo htmlspecialchars($buffer);
 					}
 					
 					fclose($handle);	
 						
-					
 				} else {
 					echo "The file $filename does not exist<br>";
 				}
-				
-		
 			}
-			
 		?>
 	</body>
 </html>
