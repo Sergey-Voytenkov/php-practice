a3cb9110332b03b47bbad465d1cfda25a2ea9dfb
SergeyV <sergey101.eco@gmail.com>
2019-11-10 10:42:04 -0500

    Did The Read Side, And Font Awesome

--- index.php
+++ index.php
@@ -3,23 +3,41 @@
 	<head>
 		<title>PHP Practice</title>
 		<link rel="stylesheet" type="text/css" href="css.css"/>
+		<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css"/>
 	</head>
 	<body>
 		<div class="main">
-			<div>
+			<form method="post">
 				<div class="fileinputs">
-					<span><input type="text" value="Type Document Name" id="DocId1"/></span>
-					<span><input type="submit"/></span>
+					<span><input type="text" placeholder="Type Document Name" name="readfile"/></span>
+					<span>
+						<button type="submit">
+							<i class="fa fa-bicycle" aria-hidden="true"></i>
+							Read
+						</button>
+					</span>
 				</div>
-				<textarea id="docshow"></textarea>
-			</div>
-			<div>
-			<div class="fileinputs2">
-					<span><input type="text" value="Type Document Name" id="DocId2"/></span>
-					<span><input type="submit"/></span>
+				<textarea id="docread"><?php
+						if(isset($_POST["readfile"])) {
+							$file = $_POST["readfile"];
+							if(file_exists($file)) {
+								$handle = fopen($file, 'r');
+								while(($buffer = fgets($handle)) !== false) {
+									echo htmlspecialchars($buffer);
+								}
+							}
+							else echo "File Not Found, Or Not Correct";
+						}
+					?></textarea>
+			</form>
+			<form method="post">
+				<div class="fileinputs">
+					<span><input type="text" placeholder="Type Document Name" name="writefile"/></span>
+					<span><input type="submit" value="Write"/></span>
 				</div>
 				<textarea id="docwrite"></textarea>
-			</div>
+			</form>
 		</div>
+		
 	</body>
 </html>
