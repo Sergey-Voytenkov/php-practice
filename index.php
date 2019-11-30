f1b8533b8fd6628f1855b9cb2ec651341771b380
SergeyV <sergey101.eco@gmail.com>
2019-11-29 21:32:23 -0500

    Did some work, they mostly work :D

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 								 name="readfile"
 								 value="<?php if(isset($_POST["readfile"])) echo $_POST["readfile"]; ?>"
 						  />
-						
 					</span>
 					<span>
 						<button name="command" value="new" type="submit">
@@ -32,25 +31,34 @@
 						if (isset($_POST['command'])) {
 							if ($_POST['command'] == 'new') {
 								//do something on New button pressed
-								$filename = $_POST["readfile"];
-								$handle = fopen($filename, 'w');
-								fwrite($handle, $_POST["docread"]);
-								fclose($handle);
+								if($_POST['readfile'] == "") echo "Please Input a File Name.";
+								else {
+									$filename = $_POST["readfile"];
+									$handle = fopen($filename, 'w');
+									fwrite($handle, $_POST["docread"]);
+									fclose($handle);
+								}
 								
 							} else if ($_POST['command'] == 'open') {								
 								//do something on Open button pressed
 								$filename = $_POST['readfile'];
 								if(file_exists($filename))
 									echo htmlspecialchars(file_get_contents($filename));
-								else echo "File Not Found, Or Not Correct";
+								else echo "File Not Found, Or Not Correct.";
 								
 							} else {
-								//do something on any other button (which is save) button pressed
+								$filename = $_POST['readfile'];
+								if(file_exists($filename)) {
+									$handle = fopen($filename, 'w');
+									fwrite($handle, $_POST['docread']);
+									fclose($handle);
+
+								}
+								else echo "File Does not Exist or is Miss Spelled.";
 							}
 						}
 					?></textarea>
 			</form>
 		</div>
-		
 	</body>
 </html>
