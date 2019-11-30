ff0b4c96f276ce3fb066752f00769b49bbfc760f
SergeyV <sergey101.eco@gmail.com>
2019-11-30 10:43:10 -0500

    Made new folder, added stuff in it, and made new index.php

--- index.php
+++ index.php
@@ -1,64 +1,24 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHP Practice</title>
-		<link rel="stylesheet" type="text/css" href="css.css"/>
-		<link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css"/>
 	</head>
 	<body>
-		<div class="main">
-			<form method="post">
-				<div class="fileinputs">
-					<span><input type="text"
-								 placeholder="Type Document Name"
-								 name="readfile"
-								 value="<?php if(isset($_POST["readfile"])) echo $_POST["readfile"]; ?>"
-						  />
-					</span>
-					<span>
-						<button name="command" value="new" type="submit">
-							<i class="fa fa-file-o" aria-hidden="true"></i>
-						</button>
-						<button name="command" value="open" type="submit">
-							<i class="fa fa-file-text" aria-hidden="true"></i>
-						</button>
-						<button name="command" value="save" type="submit">
-							<i class="fa fa-floppy-o" aria-hidden="true"></i>
-						</button>
-					</span>
-				</div>
-				<textarea name="docread"><?php				
-						if (isset($_POST['command'])) {
-							if ($_POST['command'] == 'new') {
-								//do something on New button pressed
-								if($_POST['readfile'] == "") echo "Please Input a File Name.";
-								else {
-									$filename = $_POST["readfile"];
-									$handle = fopen($filename, 'w');
-									fwrite($handle, $_POST["docread"]);
-									fclose($handle);
-								}
-								
-							} else if ($_POST['command'] == 'open') {								
-								//do something on Open button pressed
-								$filename = $_POST['readfile'];
-								if(file_exists($filename))
-									echo htmlspecialchars(file_get_contents($filename));
-								else echo "File Not Found, Or Not Correct.";
-								
-							} else {
-								$filename = $_POST['readfile'];
-								if(file_exists($filename)) {
-									$handle = fopen($filename, 'w');
-									fwrite($handle, $_POST['docread']);
-									fclose($handle);
-
-								}
-								else echo "File Does not Exist or is Miss Spelled.";
-							}
-						}
-					?></textarea>
-			</form>
-		</div>
+		<?php 
+			$vasya = 'He runs around like a waldling chickmunk.';
+			$s = serialize($vasya);
+			echo $vasya . '<br/>' . $s . '<br/>';
+			
+			$pi = 3.141592;
+			$s = serialize($pi);
+			echo $pi . '<br/>' . $s . '<br/>';
+			
+			$arr = ["Hello World 0-0", 1234567890, false];
+			$s = serialize($arr);
+			print_r($arr); 
+			echo $s . '<br/>';
+			
+			$arr2 = unserialize($s);
+			print_r($arr2);
+		?>
 	</body>
 </html>
