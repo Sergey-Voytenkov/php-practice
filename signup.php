857ec6387a9f053656482ad64ef7909e94750b71
SergeyV <sergey101.eco@gmail.com>
2019-12-14 10:44:22 -0500

    Created Sign Up Page

---
+++ signup.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<?php 
+			if(isset($_POST['username']) & isset($_POST['password'])) {
+				if(file_exists('users.txt')){
+					$contents = file_get_contents('users.txt');
+					$users = unserialize($contents);
+					$users[] = [$_POST['username'], $_POST['password'], 0];
+					$contents = serialize($users);
+					file_put_contents('users.txt', $contents);
+					echo '<p>New User was Successfully Created</p>'
+				}
+				else {
+					$arr = [$_POST['username'], $_POST['password'], 1];
+					$contents = serialize($arr);
+					file_put_contents('users.txt', $contents);
+					echo '<p>New Admin was Successfully Created</p>'
+				}
+			}
+		?>
+		<form method="POST">
+			<input type="text" name="username" placeholder="Type Your User Name Here"/>
+			<input type="password" name="password" placeholder="Type Your Password Here">
+			<input type="submit" value="Create">
+		</form>
+	</body>
+</html>
