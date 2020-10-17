7784dd35a48fba60cd65c51677bf0fb718c78e56
Sergey-Voytenkov <sergey101.eco@gmail.com>
2020-10-16 20:04:44 -0400

    Quick fix

--- admin/new_product.php
+++ admin/new_product.php
@@ -9,7 +9,7 @@
 				<label for="product_name">Name:</label>
 				<input type='text' id="product_name">
 				<br>
-				<label for="product_cost">Cost:<input type='text' id="product_cost"></p></label>
+				<label for="product_cost">Cost:<input type='text' id="product_cost"></label>
 				<input type='file' id="file"></input>
 				<div id="div-submit">
 					<input type="submit" id="submit">
